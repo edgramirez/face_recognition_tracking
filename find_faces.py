2957c045d7d6acc70c647fcbd88b6770c7c28690
edgar <edgar.ramirez@mit-mexico.com>
2021-03-11 00:16:05 -0500

    fixed path

--- find_faces.py
+++ find_faces.py
@@ -1,6 +1,6 @@
 import biblioteca as biblio
 
-image_dir = 'images/Unknown_faces/obama'
+image_dir = 'images/find'
 pickle_file = 'train.pkl'
 
 biblio.compare_pickle_against_unknown_images(pickle_file, image_dir)
