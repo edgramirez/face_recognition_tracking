3867378b1de81982c87c7a8c928520b89e58d8d0
edgar <edgar.ramirez@mit-mexico.com>
2021-03-19 23:09:46 -0400

    Considering new visit every minute

--- biblioteca.py
+++ biblioteca.py
@@ -44,7 +44,7 @@ def display_recent_visitors_face(known_face_metadata, frame):
     number_of_recent_visitors = 0
     for metadata in known_face_metadata:
         # If we have seen this person in the last minute, draw their image
-        if datetime.now() - metadata["last_seen"] < timedelta(seconds=10) and metadata["seen_frames"] > 5:
+        if datetime.now() - metadata["last_seen"] < timedelta(seconds=10) and metadata["seen_frames"] > 1:
             # Draw the known face image
             x_position = number_of_recent_visitors * 150
             frame[30:180, x_position:x_position + 150] = metadata["face_image"]
